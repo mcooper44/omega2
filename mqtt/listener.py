d0fb7597d9242a083ddec20e811112358e8dbc45
mcooper44 <m.cooper44@gmail.com>
2019-03-30 16:24:08 -0400

    pulled listener out of a class

--- mqtt/listener.py
+++ mqtt/listener.py
@@ -1,118 +1,82 @@
 from datetime import datetime, date
 import time
-import sqlite3
+import json
+
 import paho.mqtt.client as mqtt
+
 from mqtt_config import configuration
+from mqtt_database import mqtt_database
 
-INIT_STRING = """CREATE TABLE IF NOT EXISTS TempData (ID text, 
-temp real NOT NULL, date date NOT NULL)"""
-INSERT_STRING = "INSERT INTO TempData (ID, temp, date) VALUES (?, ?, ?)"
-FETCH_STRING = "SELECT from TempData WHERE ID=?"
-DB = None
+MQTT_DB = mqtt_database()
 
-class mqtt_database():
-    def __init__(self, path):
-        self.path = path
-        self.con = None
-        self.cursor = None
-        try:
-            self.con = sqlite3.connect(self.path)
-            self.cursor = self.con.cursor()
-            self.cursor.execute(INIT_STRING)
-            self.con.commit()
-        except Exception as dbase_error:
-            print(dbase_error)
+def log_to_db(database, sid, temp):
+    '''
+    passes the payload to the db for storage
+    '''
+    log_time = datetime.now()
+    database.insert_value((sid, temp, log_time))
 
-    def insert_value(self, payload):
-        '''
-        accepts a tuple and inserts it into the database
-        '''
-        self.cursor.execute(INSERT_STRING, payload)
-        self.con.commit()
-    
-    def fetch_value(self, r_value):
-        '''
-        param: r_value = tuple of strings to extract from 
-        the database
-        '''
-        self.cursor.execute(FETCH_STRING, r_value)
-        return self.cursor.fetchall()
-    
-    def shut_connection(self):
-        self.con.close()
+def on_message(client, userdata, message):
+    time.sleep(1)
+    json_payload = str(message.payload.decode("utf-8"))
+    payload = json.loads(json_payload)
+    sensor = payload['sensor']
+    reading = payload['reading']
+    print(f'{sensor} said: {reading}')
+    log_to_db(MQTT_DB, sensor, reading)
+
+def on_publish(client, userdata, mid):
+    print('message ID: {}'.format(mid))
         
-class listener():
-    def __init__(self, client_name, broker, services):
-        self.client = mqtt.Client(client_name)
-        self.broker = broker
-        self.services = services
-        self.ok_go = True
-	
-        if self.client:
-            self.client.on_message = listener.on_message
-            self.client.on_publish = listener.on_publish
+def main(client_name, broker, services):
     
-    @staticmethod
-    def log_to_db(self, database, payload):
-        '''
-	    passes the payload to the db for storage
-	    '''
-        log_time = datetime.now()
-        sid, temp = payload
+    client = mqtt.Client(client_name)
+    client.on_message = on_message
+    client.on_publish = on_publish
+    broker = broker
+    services = services
 
-        database.insert_value((sid, temp, log_time))
-    
-    @staticmethod
-    def on_message(self, client, userdata, message):
-        time.sleep(1)
-        payload = str(message.payload.decode("utf-8"))
-        print('{}'.format(payload))
-        listener.log_to_db(DB, payload)
-	
-    @staticmethod
-    def on_publish(client, userdata, mid):
-        print('message ID: {}'.format(mid))
+    '''
+    Connect to the broker so that we can subscribe 
+    and start recieving messages
+    '''
+    client.connect(broker)
+    print(f'connected to broker: {broker}')
+    time.sleep(2)
 
-    def connect(self):
-        '''
-        Connect to the broker so that we can subscribe 
-	    and start recieving messages
-        '''
-        self.client.connect(self.broker)
+    '''
+    subscribe to the services in the list
+    passed into the object when it was 
+    instantiated 
+    '''
+    for service in services:
+        client.subscribe(service)
+        print(f'subscribed to {service}')
+        time.sleep(4)
 
-    def subscribe(self):
-        '''
-	    subscribe to the services in the list
-	    passed into the object when it was 
-	    instantiated 
-	    '''
-        for service in self.services:
-            self.client.subscribe(service)
-
-    def loop(self):
-        '''
-	    Invoke this method to listen to the broker
-	    and recieve payloads from the services 
-	    that the listener connected to by invoking the 
-	    subscribe method
-	    Requires that the connect() and then subscribe()
-	    methods were previously called
-	    '''
-        try:
-            while self.ok_go:
-                self.client.loop_forever()
-        except (EOFError, KeyboardInterrupt) as e:
-            print('{}  <- this happened'.format(e))
-            self.client.loop_stop()
-            self.client.disconnect()
-            self.ok_go = False
+    '''
+    listen to the broker
+    and recieve payloads from the services 
+    that the listener connected to by invoking the 
+    subscribe method
+    Requires that a call to connect and subscribe
+    to be previously called
+    '''
+    try:
+        while True:
+            print('beginning loop')
+            client.loop_forever()
+    except (EOFError, KeyboardInterrupt) as e:
+        print('{}  <- this happened'.format(e))
+        client.loop_stop()
+        client.disconnect()
+        MQTT_DB.close_connection()
 
 if __name__ == '__main__':
-    DB = mqtt_database('swarm_log.sqlite3')
     conf = configuration('config.yaml')
+    DB = 'swarm_log.sqlite3'
     broker, services = conf.get_broker_services()
-    listen = listener('ubox', broker, services)
-    listen.connect()
-    listen.subscribe()
-    listen.loop()
-
+    client_name = conf.c_name
+    print(f'{client_name} is attempting to listen to {broker} for {services}')
+    main(client_name, broker, services)
+    
